068873c17fa4570e0904ef7d5fd41dad9e295d7f
尾ノ上裕美 <yumi@MacBook-Air-5.local>
2026-08-30 21:02:32 +0900

    Handle existing fellowship columns in deployment

--- db/migrate/20260830110500_rename_user_evangelism_meeting_to_fellowship.rb
+++ db/migrate/20260830110500_rename_user_evangelism_meeting_to_fellowship.rb
@@ -1,5 +1,5 @@
 class RenameUserEvangelismMeetingToFellowship < ActiveRecord::Migration[8.1]
   def change
-    rename_column :users, :evangelism_meeting_id, :fellowship_id
+    rename_column :users, :evangelism_meeting_id, :fellowship_id if column_exists?(:users, :evangelism_meeting_id)
   end
 end
--- db/migrate/20260830111000_rename_ceremony_schedule_evangelism_meeting_to_fellowship.rb
+++ db/migrate/20260830111000_rename_ceremony_schedule_evangelism_meeting_to_fellowship.rb
@@ -1,5 +1,5 @@
 class RenameCeremonyScheduleEvangelismMeetingToFellowship < ActiveRecord::Migration[8.1]
   def change
-    rename_column :ceremony_schedules, :evangelism_meeting_id, :fellowship_id
+    rename_column :ceremony_schedules, :evangelism_meeting_id, :fellowship_id if column_exists?(:ceremony_schedules, :evangelism_meeting_id)
   end
 end
