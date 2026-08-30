8688770efc72771567e0602facc686031e753787
尾ノ上裕美 <yumi@MacBook-Air-5.local>
2026-08-30 12:23:10 +0900

    Allow blank ceremony schedule minister

--- app/models/ceremony_schedule.rb
+++ app/models/ceremony_schedule.rb
@@ -1,7 +1,7 @@
 class CeremonySchedule < ApplicationRecord
   belongs_to :fellowship
 
-  validates :ceremony_at, :place, :assistant_count, :spirit_count, :minister_name, presence: true
+  validates :ceremony_at, :place, :assistant_count, :spirit_count, presence: true
   validates :assistant_count, :spirit_count, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   scope :chronological, -> { includes(:fellowship).order(:ceremony_at, :id) }
--- test/models/ceremony_schedule_test.rb
+++ test/models/ceremony_schedule_test.rb
@@ -12,8 +12,19 @@ class CeremonyScheduleTest < ActiveSupport::TestCase
     assert_not schedule.valid?
     assert_includes schedule.errors[:ceremony_at], "can't be blank"
     assert_includes schedule.errors[:place], "can't be blank"
-    assert_includes schedule.errors[:minister_name], "can't be blank"
     assert schedule.errors[:assistant_count].any?
     assert schedule.errors[:spirit_count].any?
+  end
+
+  test "allows the minister name to be blank" do
+    schedule = CeremonySchedule.new(
+      fellowship: @meeting,
+      ceremony_at: Time.zone.local(2026, 8, 30, 10, 0),
+      place: "大江戸会館",
+      assistant_count: 1,
+      spirit_count: 10
+    )
+
+    assert_predicate schedule, :valid?
   end
 end
